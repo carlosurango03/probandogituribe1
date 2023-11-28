b0caf0c1546382d42999c2b7733981b8a797309d
sntyart5 <141444070+pablolotero@users.noreply.github.com>
2023-11-28 11:33:42 -0500

    se inicia preoyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+        //Natillera la abuela toretto
+        //¿Que necesito para procesar la natillera de 1 cliente'
+       //ENTRADAS
+    int identidificadorCliente=233444;
+    long documentoIdentidad=1193096819222L;
+    byte edadCliente=34;
+    short cuotaMensualCliente=25000;
+    float tasaInteresFijo=0.1F;
+    char calificacionCliente='A';
+
+
+
+
+
+}
